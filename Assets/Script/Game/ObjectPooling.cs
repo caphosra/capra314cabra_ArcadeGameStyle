357b117f4fd5fdea7d97a0f84a012d4657a26139
capra314cabra <capra314cabra@gmail.com>
2018-10-14 10:34:45 +0900

    ObjectPoolingの準備

---
+++ Assets/Script/Game/ObjectPooling.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject obj;
+
+    [SerializeField]
+    private int startCount;
+
+    private List<GameObject> objects;
+
+    private void Start()
+    {
+        for(int i = 0; i < startCount; i++)
+        {
+            CreateNew(false);
+        }
+    }
+
+    public void CreateNew(bool active)
+    {
+        var go = Instantiate(obj);
+        go.SetActive(active);
+        objects.Add(go);
+    }
+
+    public GameObject GetObject()
+    {
+        foreach(var go in objects)
+        {
+            if(!go.activeSelf)
+            {
+                go.SetActive(true);
+                return go;
+            }
+        }
+        CreateNew(true);
+        return objects[objects.Count - 1];
+    }
+}

332f2829a96ac4ae8ccef25e06f8f3dce75a57bb
capra314cabra <capra314cabra@gmail.com>
2018-10-28 13:54:54 +0900

    無限ループをしないように設定

--- Assets/Script/GameOver/GameOverManager.cs
+++ Assets/Script/GameOver/GameOverManager.cs
@@ -24,6 +24,6 @@ public class GameOverManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return)) SceneManager.LoadScene(titleScene);
+        
     }
 }
